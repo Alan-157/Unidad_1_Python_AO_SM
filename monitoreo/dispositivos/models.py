d0e193b9dddcb12865fe11ca89beb62432825a86
Alan-157 <alonzooviedo157@gmail.com>
2025-09-09 12:12:33 -0300

    Tercer commit, hechos los modelos con nombre en ingles

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Zone(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True, help_text="Zone description.")
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    power_consumption = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Measurement(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    consumption_w = models.FloatField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Measurement for {self.device.name} - {self.consumption_w}W"
+
+class Alert(models.Model):
+    measurement = models.OneToOneField(Measurement, on_delete=models.CASCADE)
+    message = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(default=timezone.now)
+    reviewed = models.BooleanField(default=False, help_text="Indicates if the alert has been reviewed.")
+
+    def __str__(self):
+        return f"Alert for {self.measurement.device.name}"
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
